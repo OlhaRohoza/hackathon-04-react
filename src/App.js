dc06760b86e55bb3dc6a2dfe2b444f97cfdf23a8
harisrahan9249 <harisrahman9249@gmail.com>
2022-07-01 12:22:59 +0200

    flight table

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import logo from './logo.svg';
+
+
+
 import './App.css';
 import { useState, useEffect } from 'react';
+import FlightTable from './FlightTable';
 
 function App() {
-
+  const [flights,setFlights] = useState ([]);
   const flyFrom = 'PRG';
   const flyTo = 'VLC';
 
   const partner = 'data4youcbp202106';
   const url = `https://api.skypicker.com/flights?fly_from=${flyFrom}&fly_to=${flyTo}&partner=${partner}`;
 
-  const fetchData = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-
-  }
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data)
+       return setFlights(data.data);
+    } catch(err) {
+      console.log(err);
+    }
+}
+    
 
   useEffect(() => {
-    fetchData();
-  }, [])
+    fetchData(url);
+  }, []);
 
 
   return (
     <div className="App">
       <h1>flights is loading</h1>
+      
+     
+        <FlightTable  flights ={flights} /> 
+        
+
+       
+      
     </div>
   );
 }
---
+++ src/FlightTable.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { DateTime } from 'luxon';
+ import './Card.css';
+function FlightTable({flights}) {
+  return (
+    <div>
+      <table>
+      <thead>
+        <th>Price</th>
+        <th>Name of origin</th>
+        <th>Code of the origin</th>
+        <th>Destination city</th>
+        <th>Destination</th>
+        <th>Date and Time of Departure</th>
+        <th>Date and Time of Arrival to Destination </th>
+      </thead>
+      <tbody>
+      { flights && (flights.map((flight,i)=>( 
+         <tr>
+          
+         <td>{flight.price} </td>
+         <td>{flight.cityFrom} </td>
+         <td>{flight.flyFrom} </td>
+         <td>{flight.cityTo} </td>
+         <td>{flight.flyTo} </td>
+         <td>{DateTime.fromMillis(flight.dTime * 1000).toFormat('hh:mm')} </td>
+         <td>{DateTime.fromMillis(flight.aTime * 1000).toFormat('hh:mm')}</td>
+       </tr> 
+        
+
+       )))} 
+        
+      </tbody>
+       
+      </table>
+        
+      
+    </div>
+  )
+}
+
+export default FlightTable
